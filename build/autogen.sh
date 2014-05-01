77fad52eb38669fee4093c1d96578919053c10d4
rkeene <rkeene@noemail.net>
2014-05-01 21:50:31 +0000

    Moved M4 fetching to autogen
    FossilOrigin-Name: 131645cec4754221a3680f4f8216b73c04858de5

--- build/autogen.sh
+++ build/autogen.sh
@@ -1,5 +1,23 @@
 #! /bin/bash
 
+# Download latest copy of autoconf macros
+(
+	cd aclocal || exit 1
+
+	for file in shobj.m4 tcl.m4 versionscript.m4; do
+		rm -f "${file}"
+
+		wget -O "${file}.new" "http://rkeene.org/devel/autoconf/${file}.m4" || continue
+
+		mv "${file}.new" "${file}"
+	done
+)
+
 aclocal -I aclocal
 autoconf
 rm -rf autom4te.cache
+
+for file in config.guess config.sub install-sh; do
+	...
+done
+
