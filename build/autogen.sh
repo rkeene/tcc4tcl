2cc31ab86ec22a25cc02bfb7e8642bee8bb97fc5
rkeene <rkeene@noemail.net>
2014-05-01 21:56:10 +0000

    Updated to generate files
    FossilOrigin-Name: 4d5caafa3d5025c1555f3e0d4304c4d94a47b5c6

--- build/autogen.sh
+++ build/autogen.sh
@@ -7,17 +7,18 @@
 	for file in shobj.m4 tcl.m4 versionscript.m4; do
 		rm -f "${file}"
 
-		wget -O "${file}.new" "http://rkeene.org/devel/autoconf/${file}.m4" || continue
+		wget -O "${file}.new" "http://rkeene.org/devel/autoconf/${file}" || continue
 
 		mv "${file}.new" "${file}"
 	done
 )
 
-aclocal -I aclocal
-autoconf
-rm -rf autom4te.cache
-
 for file in config.guess config.sub install-sh; do
-	...
+	rm -f "${file}"
 done
 
+aclocal -I aclocal
+autoconf
+automake -fca
+
+rm -rf autom4te.cache
