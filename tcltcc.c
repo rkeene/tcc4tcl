d7464f4476641a12a24a5bd8342792c76bb913e8
rkeene <rkeene@noemail.net>
2014-05-01 23:32:44 +0000

    Updated to build Tcc naively
    FossilOrigin-Name: a319c993d903aaf56c78dfaeb18746553a61b206

--- tcltcc.c
+++ tcltcc.c
@@ -18,6 +18,9 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
+#include <tcl.h>
+#include "tcc.h"
+
 static void TccErrorFunc(Tcl_Interp * interp, char * msg) {
     Tcl_AppendResult(interp, msg, "\n", NULL);
 }
