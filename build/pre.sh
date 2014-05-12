5e2e9b7eed019930a15a091faa962257e047b613
rkeene <rkeene@noemail.net>
2014-05-12 03:19:30 +0000

    Updated to fallback to SHA1 if SHA256 is unavailable
    FossilOrigin-Name: 3c8c30b6bf1383e7cac11e2af2a9131bf3a43eb8

--- build/pre.sh
+++ build/pre.sh
@@ -9,16 +9,24 @@ cd "$(dirname "$(which "$0")")/.." || exit 1
 tcc_version='0.9.26'
 tcc_url="http://download.savannah.gnu.org/releases/tinycc/tcc-${tcc_version}.tar.bz2"
 tcc_sha256='521e701ae436c302545c3f973a9c9b7e2694769c71d9be10f70a2460705b6d71'
+tcc_sha1='7110354d3637d0e05f43a006364c897248aed5d0'
 (
 	rm -rf __TMP__
 	mkdir __TMP__ || exit 1
 	cd __TMP__ || exit 1
 
 	wget -O 'new' "${tcc_url}" || rm -f new
-	new_sha256="$(openssl sha256 new | sed 's@.*= *@@')"
+	new_hash="$(openssl sha256 new 2>/dev/null | sed 's@.*= *@@')"
 
-	if [ "${new_sha256}" != "${tcc_sha256}" ]; then
-		echo "Checksum Mismatch: Downloaded: ${new_sha256}; Expected: ${tcc_sha256}" >&2
+	if [ -z "${new_hash}" ]; then
+		new_hash="$(openssl sha1 new 2>/dev/null | sed 's@.*= *@@')"
+		check_hash="${tcc_sha1}"
+	else
+		check_hash="${tcc_sha256}"
+	fi
+
+	if [ "${new_hash}" != "${check_hash}" ]; then
+		echo "Checksum Mismatch: Downloaded: ${new_hash}; Expected: ${check_hash}" >&2
 
 		rm -f new
 
